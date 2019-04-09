c82be6bb8ba81ec590a9c351e001bef86cd9bf6f
Simon J <sj.hoffiz@gmail.com>
2019-04-08 23:19:47 -0400

    Quick error fix.

--- js/UI.js
+++ js/UI.js
@@ -1,8 +1,9 @@
 console.log ("UI");
 
+/* can delete, attempt at loading heart image for player life
 function playerLife () {
-canvasContext.drawImage(playerLifeIcon,canvas.width/2 ,canvas.height/2);
+canvasContext.drawImage(playerLifeIcon, canvas.width/2 ,canvas.height/2);
 }
 
 playerLife ();
-
+*/
