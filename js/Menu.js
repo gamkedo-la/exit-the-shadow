738f3d32cf8e40f9c064557419633d56984dd3dc
Vaan Hope Khani <e.t.khani@gmail.com>
2019-04-28 19:18:12 -0700

    Updating menu a bit.

--- js/Menu.js
+++ js/Menu.js
@@ -24,4 +24,59 @@ let classListHelp= ["gameplay","gamepad","back"];
 let classListPaused= ['save' , 'audio',  'back'];
 let classListCredits= ["back"];
 
+
+this.menuMouse = function(){
+
+}
+
+this.update = function(){
+
+}
+
+this.checkState = function(){
+    if(currentPage == HELP_PAGE){
+
+    }  
+}
+
+
+this.redraw = function(){
+canvasContext,save;
+canvasContext.setTransform(1, 0, 0, 0, 1, 0, 0, 0);
+canvasContext.clearReact(0, 0, canvas.width, canvas.height);
+canvasContext.restore;
+}
+
+this.draw = function(){
+    if(gameIsStarted === false)
+    {
+        if(currentPage == PAUSED_PAGE)
+        {
+          currentPage = MENU_PAGE;
+        }
+
+        this.redraw;
+
+        canvasContext.drawImage(titlePic, 0,0);
+        if(currentPage != CREDITS_PAGE && currentPage != HELP_PAGE) {
+            canvasContext.drawImage(logoPic, canvas.width/2);
+        }
+    }
+    
+    else {return;};
+
+    for (let i = 0; i<menuPageText().length; i++)
+    {
+        drawText(menuPageText[currentPage][i]), itemsX - (currentPage== HELP_PAGE), topItemY+rowHeight*i - (currentPage ==HELP_PAGE);
+
+        //Draw cursor
+        if (curentPage !=HELP_PAGE){
+            canvas.canvasContext(arrowPic, itemsX, topItemY + (this.cursor1 * rowHeight));
+        }
+
+    }
+}
+
+
+
 })();
