01605b660700ed242f76b02d9329cba1b121f229
Praneil Kamat <pkamat@mac.com>
2019-05-04 07:10:42 +0100

    Integrate player attack sprites with shadows

--- js/ImageLoading.js
+++ js/ImageLoading.js
@@ -29,7 +29,7 @@ function loadImage(imgVar, fileName) {
 
 function loadImages() {
 	var imageList = [
-		{varName: playerSheet, fileName: "player_sheet_no_shadows.png"},
+		{varName: playerSheet, fileName: "player_sheet.png"},
 		{varName: beastSheet, fileName: "BeastSpriteSheet.png"},
 		{varName: shadowSheet, fileName: "ShadowSpriteSheet.png"},
 		{varName: evilPlayerSheet, fileName: "EvilPlayerSheet.png"},
