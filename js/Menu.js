02f7f2b8f078547558b13b586215d2f45ef7d39c
Praneil Kamat <pkamat@mac.com>
2019-06-09 15:49:18 +0900

    Clean up menu code, make text a little larger, correct cursor positioning, improve click box accuracy

--- js/Menu.js
+++ js/Menu.js
@@ -1,149 +1,121 @@
 const Menu = new (function() {
-const MENU_PAGE = 0;
-const SETTINGS_PAGE = 1;
-const HELP_PAGE = 2;
-const CREDITS_PAGE = 3;
-//const RESUME_PAGE = 1;
-//const PAUSED_PAGE = 2;
-
-
-let itemsX = 310;
-let topItemY = 390;
-let itemsWidth = 650;
-let rowHeight = 46;
-
-this.cursor1 = null;
-let currentPage = 0;
-
-let textFontFace = "18px Satisfy";
-let textFontFaceCredits = "28px Satisfy";
-let textColour = "white" ;
-
-let classListMenu = ["New Game", "Continue", "Settings" , "Help", "Credits"];
-let classListSettings = ["volume", "controls", "Back"];
-let classListHelp= ["gameplay","gamepad","Back"];
-let classListPaused= ['save' , 'audio',  'Back'];
-let classListCredits= ["Back"];
-
-let menuPageText = [classListMenu, classListSettings, classListHelp, classListCredits, classListPaused];
-
-this.menuMouse = function(){
-    for (let i=0; i<menuPageText[currentPage].length; i++){
-        if(mouseX > itemsX && mouseX < itemsX + itemsWidth && mouseY > topItemY + (i*rowHeight)-50 &&
-         mouseY < topItemY + (i+1) * rowHeight - 50){
-            this.cursor1= i;
-        } 
-    }
-}
-
-this.update = function(){
-    if (this.cursor1 < 0){
-        this.cursor1 = menuPageText[currentPage].length -1;
-    }
-
-    if (this.cursor1 > menuPageText[currentPage].length){
-        this.cursor1 = 0;
-    }
-
-    this.menuMouse();
-    this.draw();
-}
-
-this.checkState = function(){
-    if(currentPage == HELP_PAGE){
-        currentPage = MENU_PAGE;
-        this.cursor = 0;
-        return;
-    }  
-}
-
-this.changeMenuStateOnClick = function() {
-	//console.log(mouseX + " , " + mouseY + " , " +  Menu.cursor1);
-	if (gameIsStarted) { 
-		return;
+	const MENU_PAGE = 0;
+	const SETTINGS_PAGE = 1;
+	const HELP_PAGE = 2;
+	const CREDITS_PAGE = 3;
+	
+	let itemsX = 250;
+	let topItemY = 300;
+	let itemsWidth = 650;
+	let rowHeight = 80;
+	
+	this.cursor1 = null;
+	let currentPage = 0;
+	
+	let textFontFace = "32px Satisfy";
+	let textFontFaceCredits = "28px Satisfy";
+	let textColour = "white";
+	
+	let classListMenu = ["New Game", "Continue", "Settings" , "Help", "Credits"];
+	let classListSettings = ["Volume", "Back"];
+	let classListHelp = ["Controls","Back"];
+	let classListCredits = ["Back"];
+	
+	let menuPageText = [classListMenu, classListSettings, classListHelp, classListCredits];
+	
+	this.menuMouse = function(){
+	    for (let i=0; i<menuPageText[currentPage].length; i++){
+	        if(mouseX > itemsX && mouseX < itemsX + itemsWidth && mouseY > topItemY + (i*rowHeight) - (rowHeight / 2) &&
+	         mouseY < topItemY + (i+1) * rowHeight - (rowHeight / 2)){
+	            this.cursor1 = i;
+	        } 
+	    }
+		
+	    if (this.cursor1 < 0){
+	        this.cursor1 = menuPageText[currentPage].length -1;
+	    }
+	
+	    if (this.cursor1 > menuPageText[currentPage].length){
+	        this.cursor1 = 0;
+	    }
 	}
-
-	switch(menuPageText[currentPage][Menu.cursor1]) {
-
-	//MENU PAGE
-	case "New Game":
-	    gameIsStarted = true;
-	    currentBackgroundMusic = AMBIENT_MUSIC;
-	    bg_music[currentBackgroundMusic].play();
-	    this.cursor1 = null;
-	    break;
-	case "Continue":
-		if (listSaves() == undefined || listSaves() == null) {
-			console.log("No save games found");
+	
+	this.update = function(){
+	    this.menuMouse();
+	    this.draw();
+	}
+	
+	this.changeMenuStateOnClick = function() {
+		if (gameIsStarted) { 
+			return;
+		}
+	
+		switch(menuPageText[currentPage][Menu.cursor1]) {
+	
+		//MENU PAGE
+		case "New Game":
+		    gameIsStarted = true;
+		    currentBackgroundMusic = AMBIENT_MUSIC;
+		    bg_music[currentBackgroundMusic].play();
+		    this.cursor1 = null;
+		    break;
+		case "Continue":
+			if (listSaves() == undefined || listSaves() == null) {
+				console.log("No save games found");
+		    	return;
+	        }
+	        gameIsStarted = true;
+		    currentBackgroundMusic = AMBIENT_MUSIC;
+		    bg_music[currentBackgroundMusic].play();
+		    loadGame();
+		    Menu.cursor1 = null;
+		    break;
+		case "Settings":
+		    Menu.cursor1 = null;
+		    currentPage = SETTINGS_PAGE;
+		    break;
+		case "Help":
+		    Menu.cursor1 = null;
+		    currentPage  = HELP_PAGE;
+		    break;
+		case "Credits":
+		    Menu.cursor1 = null;
+		    currentPage  = CREDITS_PAGE;
+		    break;
+		case "Back":
+		    Menu.cursor1 = null;
+		    currentPage  = MENU_PAGE;
+		    break;
+		}
+	}
+	
+	this.redraw = function(){ 
+		canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+	}
+	
+	this.draw = function(){
+	    if(gameIsStarted) {
 	    	return;
-        }
-        gameIsStarted = true;
-	    currentBackgroundMusic = AMBIENT_MUSIC;
-	    bg_music[currentBackgroundMusic].play();
-	    loadGame();
-	    this.cursor1 = null;
-	    break;
-	case "Settings":
-	    this.cursor1 = null;
-	    currentPage = SETTINGS_PAGE;
-	    break;
-	case "Help":
-	    this.cursor1 = null;
-	    currentPage  = HELP_PAGE;
-	    break;
-	case "Credits":
-	    this.cursor1 = null;
-	    currentPage  = CREDITS_PAGE;
-	    break;
-	case "Back":
-	    this.cursor1 = null;
-	    currentPage  = MENU_PAGE;
-	    break;
-	}
-}
+	    }
+	
+	    this.redraw();
+	
+	    canvasContext.drawImage(titlePic, 0,0);
+	    if(currentPage != CREDITS_PAGE && currentPage != HELP_PAGE) {
+	        canvasContext.drawImage(logoPic, canvas.width/2,30, 300,300);
+	    }
+		
+	    for (let i = 0; i<menuPageText[currentPage].length; i++)
+	    {
+	        canvasContext.save();
+	        canvasContext.font = textFontFace;
+	        colorText(menuPageText[currentPage][i],itemsX - (currentPage == CREDITS_PAGE ? 275 : 0),topItemY + rowHeight * i - (currentPage == CREDITS_PAGE ? 325 : 0),
+	        (currentPage == CREDITS_PAGE ? "yellow" : textColour), (currentPage == CREDITS_PAGE ? textFontFace : textColour), 'left', 'top');
 
-this.redraw = function(){ 
- 	//canvasContext.save;
-	// canvasContext.setTransform(1, 0, 0, 0, 1, 0, 0, 0);
-	canvasContext.clearRect(0, 0, canvas.width, canvas.height);
-	//canvasContext.restore;
-}
-
-this.draw = function(){
-    if(gameIsStarted === false)
-    {
-        // if(currentPage == PAUSED_PAGE)
-        // {
-        //   currentPage = MENU_PAGE;
-        // }
-
-        this.redraw();
-
-        canvasContext.drawImage(titlePic, 0,0);
-        if(currentPage != CREDITS_PAGE && currentPage != HELP_PAGE) {
-            canvasContext.drawImage(logoPic, canvas.width/2,30, 300,300);
-        }
-    }
-    
-    else {return;};
-
-
-    for (let i = 0; i<menuPageText[currentPage].length; i++)
-    {
-        canvasContext.save();
-        canvasContext.font = textFontFace;
-        colorText(menuPageText[currentPage][i],itemsX - (currentPage == CREDITS_PAGE ? 275 : 0),topItemY + rowHeight * i - (currentPage == CREDITS_PAGE ? 325 : 0),
-        (currentPage == CREDITS_PAGE ? "yellow" : textColour), (currentPage == CREDITS_PAGE ? textFontFace : textColour), 'left', 'top');
-
-        //Draw cursor
-        if (currentPage !=HELP_PAGE){
-            canvasContext.drawImage(arrowPic, itemsX, topItemY + (this.cursor1 * rowHeight));
-        }
-
-    }
-}
-
-
-
-
+	        //Draw cursor
+	        canvasContext.drawImage(arrowPic, itemsX - 30, topItemY + (this.cursor1 * rowHeight) - 25);
+			
+			canvasContext.restore();
+	    }
+	}	
 })();
